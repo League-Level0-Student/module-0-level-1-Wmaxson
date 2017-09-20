2aca30fed4ca284cec0d95c7cefa90bbaff079ec
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-19 19:28:04 -0700

    FindTheError SafeCracker

--- src/SafeCracker.java
+++ src/SafeCracker.java
@@ -16,11 +16,10 @@ import javax.swing.JOptionPane;
 public class SafeCracker {
 
 	public static void main(String[] args) {
-		/*
-		 * Your mission: use the tryCode method to crack the safe by trying all
-		 * possible combinations
-		 */
-
+		for (int i = 0; i <= 9999999; i++) {
+		
+		tryCode(i);
+	}
 	}
 
 	static void tryCode(int guess) {
--- src/ShinyObjects.java
+++ src/ShinyObjects.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -7,31 +8,36 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 public class ShinyObjects {
 
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+		String Shinies = JOptionPane.showInputDialog(null, "How many SHINYs do you want?");
+		int SHINY = Integer.parseInt(Shinies);
 		// 3. Play the sound that many times
+		for (int i = 0; i < SHINY; i++) {
+			playMisterZee();
+		}
+	
 
-		// 1. Call the method below
-
+	// 1. Call the method below
 
 	}
 
 	public static void playMisterZee() {
-    	try {
-   		 File soundFile = new File("/Users/League/Google Drive/league-sounds/shiny-objects.wav");
-   		 AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-   		 Clip clip = AudioSystem.getClip();
-   		 clip.open(audioInputStream);
-   		 clip.start();
-   		 Thread.sleep(3400);
-    	} catch (Exception ex) {
-        	ex.printStackTrace();
-    	}
+		try {
+			File soundFile = new File("/Users/League/Google Drive/league-sounds/shiny-objects.wav");
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
